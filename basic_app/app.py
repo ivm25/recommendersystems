324d47916a2525cefb8980b57fd644a3a4e4264c
ivm25 <ishanvm25@gmail.com>
2023-09-20 16:54:17 +1000

    updated the environment with matplotlib and seaborn

--- basic_app/app.py
+++ basic_app/app.py
@@ -1,9 +1,11 @@
 from shiny import App, render, ui
+from matplotlib import pyplot as plt
 
 app_ui = ui.page_fluid(
-    ui.h2("Hello world of Recommender Systems!"),
+    ui.h2("Hello world of Rec Systems"),
     ui.input_slider("n", "N", 0, 140, 20),
     ui.output_text("txt"),
+    ui.output_plot("test_plot"),
 )
 
 
@@ -12,6 +14,9 @@ def server(input, output, session):
     @render.text
     def txt():
         return f"n*2 is {input.n() * 2}"
-
+    @output
+    @render.plot
+    def test_plot():
+        return plt.scatter([1,2,3],[4,4,7])
 
 app = App(app_ui, server)
